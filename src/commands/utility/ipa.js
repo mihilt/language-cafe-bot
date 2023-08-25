a3c26b27f475eeeaca30e5f067c981e6ad8ebcb9
mihilt <kwschic@gmail.com>
2023-08-25 22:11:49 +0900

    feat: add error handling in IPA feature

--- src/commands/utility/ipa.js
+++ src/commands/utility/ipa.js
@@ -12,30 +12,44 @@ export default {
   async execute(interaction) {
     const input = interaction.options.getString('input');
 
-    const wiktionaryRes = await axios.get(`https://en.wiktionary.org/wiki/${input}`);
+    try {
+      const wiktionaryRes = await axios.get(`https://en.wiktionary.org/wiki/${input}`);
 
-    const { window } = new JSDOM(wiktionaryRes.data);
-    const { document } = window;
-    const ipaElements = document.getElementsByClassName('IPA');
+      const { window } = new JSDOM(wiktionaryRes.data);
+      const { document } = window;
+      const ipaElements = document.getElementsByClassName('IPA');
 
-    const ipaArray = [];
-    for (let i = 0; i < ipaElements.length; i++) {
-      ipaArray.push(ipaElements[i].textContent);
+      if (!ipaElements.length) throw new Error();
+
+      const ipaArray = [];
+      for (let i = 0; i < ipaElements.length; i++) {
+        ipaArray.push(ipaElements[i].textContent);
+      }
+
+      ipaArray.forEach((ipa, index) => {
+        // eslint-disable-next-line no-useless-escape
+        ipaArray[index] = ipa.replace(/[\/\[\]]/g, '');
+      });
+
+      const ipaContent = ipaArray.map((ipa, index) => `${index + 1}. ${ipa}`);
+
+      const embed = {
+        color: 0x65a69e,
+        title: `IPA for ${input}`,
+        description: ipaContent.join('\n'),
+      };
+
+      await interaction.reply({ embeds: [embed] });
+    } catch (error) {
+      await interaction.reply({
+        embeds: [
+          {
+            color: 0x65a69e,
+            title: 'No IPA found.',
+          },
+        ],
+        ephemeral: true,
+      });
     }
-
-    ipaArray.forEach((ipa, index) => {
-      // eslint-disable-next-line no-useless-escape
-      ipaArray[index] = ipa.replace(/[\/\[\]]/g, '');
-    });
-
-    const ipaContent = ipaArray.map((ipa, index) => `${index + 1}. ${ipa}`);
-
-    const embed = {
-      color: 0x65a69e,
-      title: `IPA for ${input}`,
-      description: ipaContent.join('\n'),
-    };
-
-    await interaction.reply({ embeds: [embed] });
   },
 };
