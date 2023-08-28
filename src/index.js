6303e0ecaa129d92adeaebf1badc83f2869ee32c
mihilt <kwschic@gmail.com>
2023-08-28 21:44:58 +0900

    feat: make thread and react in specific channel

--- src/index.js
+++ src/index.js
@@ -133,4 +133,18 @@ client.on(Events.InteractionCreate, async (interaction) => {
   }
 });
 
+client.on('messageCreate', async (message) => {
+  if (message.channel.id === '739915251864043640') {
+    await message.channel.threads.create({
+      name: 'New Thread',
+      // seven days
+      autoArchiveDuration: 10080,
+      startMessage: message.id,
+    });
+
+    await message.react('783705863381975070');
+    await message.react('783705940230144001');
+  }
+});
+
 client.login(config.DISCORD_TOKEN);
