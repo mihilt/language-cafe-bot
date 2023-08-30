fcaf0037ba92ad7b025de712f10c3da46aad8a68
mihilt <kwschic@gmail.com>
2023-08-30 21:00:56 +0900

    fix: separate InteractionCreate event
    - use dynamic event file loader function
    - export client instance for use in other modules

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-import { Client, Collection, Events, GatewayIntentBits } from 'discord.js';
+import { Collection } from 'discord.js';
 import fs from 'fs';
 import { readFile } from 'node:fs/promises';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import client from './client/index.js';
 import keyv from './db/keyv.js';
-import GeneratePollChatInputCommand from './interaction/chat-input-command/generate-poll.js';
-import GeneratePollModalSubmit from './interaction/modal-submit/generate-poll.js';
-import suggestionBoxMessageCreate from './messageCreate/suggestion-box.js';
 
 const configUrl =
   process.env.NODE_ENV === 'production' ? './config/config_pro.json' : './config/config_dev.json';
@@ -16,14 +14,6 @@ const config = JSON.parse(await readFile(fileUrl, 'utf8'));
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-  ],
-});
 
 client.cooldowns = new Collection();
 client.commands = new Collection();
@@ -76,85 +66,5 @@ for (const file of eventFiles) {
     }
   })();
 }
-
-client.on(Events.InteractionCreate, async (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
-
-  const command = interaction.client.commands.get(interaction.commandName);
-
-  if (!command) {
-    console.error(`No command matching ${interaction.commandName} was found.`);
-    return;
-  }
-
-  const { cooldowns } = client;
-
-  if (!cooldowns.has(command.data.name)) {
-    cooldowns.set(command.data.name, new Collection());
-  }
-
-  const now = Date.now();
-  const timestamps = cooldowns.get(command.data.name);
-  const defaultCooldownDuration = 5;
-  const cooldownAmount = (command.cooldown ?? defaultCooldownDuration) * 1000;
-
-  if (timestamps.has(interaction.user.id)) {
-    const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
-
-    if (now < expirationTime) {
-      const expiredTimestamp = Math.round(expirationTime / 1000);
-      interaction.reply({
-        content: `Please wait, you are on a cooldown for \`${command.data.name}\`. You can use it again <t:${expiredTimestamp}:R>.`,
-        ephemeral: true,
-      });
-      return;
-    }
-  }
-
-  timestamps.set(interaction.user.id, now);
-  setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
-
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    if (interaction.replied || interaction.deferred) {
-      await interaction.followUp({
-        content: 'There was an error while executing this command!',
-        ephemeral: true,
-      });
-    } else {
-      await interaction.reply({
-        content: 'There was an error while executing this command!',
-        ephemeral: true,
-      });
-    }
-  }
-});
-
-client.on(Events.InteractionCreate, async (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
-
-  if (interaction.commandName === 'generate-poll') {
-    GeneratePollChatInputCommand(interaction);
-  }
-});
-
-// Listen for the modal submit event
-client.on(Events.InteractionCreate, async (interaction) => {
-  if (!interaction.isModalSubmit()) return;
-  if (interaction.customId === 'generate-poll') {
-    GeneratePollModalSubmit(interaction);
-  }
-});
-
-client.on(Events.MessageCreate, async (message) => {
-  if (message.author.bot) return;
-
-  // if channel is #suggention-box, create a thread and react
-  if (message.channel.id === '739915251864043640') {
-    suggestionBoxMessageCreate(message);
-  }
-});
 
 client.login(config.DISCORD_TOKEN);
