79cd340151d812a6c540914d07c28e321b904b30
mihilt <kwschic@gmail.com>
2023-08-31 20:47:48 +0900

    feat: add etuymology feature

---
+++ src/commands/utility/etuymology.js
@@ -0,0 +1,124 @@
+import axios from 'axios';
+import { SlashCommandBuilder } from 'discord.js';
+import { JSDOM } from 'jsdom';
+import channelLog, { generateInteractionMessage } from '../../util/channel-log.js';
+
+const data = new SlashCommandBuilder()
+  .setName('etymology')
+  .setDescription('Get the etymology of a word')
+  .addStringOption((option) =>
+    option
+      .setName('input')
+      .setMaxLength(20)
+      .setDescription('The word to get the etymology of')
+      .setRequired(true),
+  );
+
+export default {
+  data,
+  async execute(interaction) {
+    await interaction.deferReply();
+    const input = interaction.options.getString('input');
+
+    channelLog(generateInteractionMessage(interaction, `input: ${input}`));
+
+    const inputForUrl = input.replace(/ /g, '_');
+
+    try {
+      const url = `https://en.wiktionary.org/wiki/${inputForUrl}`;
+
+      const wiktionaryRes = await axios.get(url);
+
+      const dom = new JSDOM(wiktionaryRes.data);
+
+      const { window } = dom;
+      const { document } = window;
+
+      const h2Elements = document.querySelector('.mw-parser-output').querySelectorAll('H2');
+
+      const extractedGroups = [];
+
+      h2Elements.forEach((h2, index) => {
+        const nextH2 = h2Elements[index + 1]; // Get the next h2 element
+
+        let currentElement = h2.nextSibling;
+        const groupFragment = dom.window.document.createDocumentFragment();
+
+        // Loop until we reach the next h2 or null
+        while (currentElement !== nextH2 && currentElement !== null) {
+          if (currentElement.nodeType === dom.window.Node.ELEMENT_NODE) {
+            groupFragment.appendChild(currentElement.cloneNode(true));
+          }
+          currentElement = currentElement.nextSibling;
+        }
+
+        if (groupFragment.childNodes.length > 0) {
+          extractedGroups.push(groupFragment);
+        }
+      });
+
+      const languageGroup = [];
+
+      extractedGroups.forEach((group, index) => {
+        if (
+          group.querySelector('[id^="Etymology"]') ||
+          h2Elements[index].querySelector('.mw-headline')?.textContent
+        ) {
+          const etymologyElements = group.querySelectorAll('[id^="Etymology"]');
+          const parentsNextElement = [];
+
+          etymologyElements.forEach((element) => {
+            parentsNextElement.push(element.parentElement.nextElementSibling);
+          });
+
+          // filter parentsNextElement is only p tag
+          const filteredParentsNextElementTextContent = parentsNextElement
+            .filter((element) => element.tagName === 'P')
+            .map((element) => element.textContent);
+
+          languageGroup.push({
+            language: h2Elements[index].querySelector('.mw-headline').textContent,
+            etymology: filteredParentsNextElementTextContent,
+          });
+        }
+      });
+
+      const filteredLanguageGroup = languageGroup.filter((group) => group.etymology.length);
+
+      let content = filteredLanguageGroup
+        .map((group) => `**${group.language}**\n${group.etymology.join('\n')}`)
+        .join('\n');
+
+      if (content === '') {
+        content +=
+          "It looks like the Wiktionary page for the word you entered doesn't have an etymology available.\n\n";
+        content += `However, you can visit the word's Wiktionary page by clicking [here](${url}) for additional information.`;
+      }
+
+      content += `\n[See more on Wiktionary](${url})`;
+
+      const embed = {
+        color: 0x65a69e,
+        title: `Etymology for ${input}`,
+        description: content,
+      };
+
+      await interaction.editReply({ embeds: [embed] });
+    } catch (error) {
+      if (error.response && error.response.status === 404) {
+        await interaction.editReply({
+          embeds: [
+            {
+              color: 0x65a69e,
+              title: 'No etymology found.',
+              description: 'Please check your spelling and try again.',
+            },
+          ],
+          ephemeral: true,
+        });
+        return;
+      }
+      console.error(error);
+    }
+  },
+};
