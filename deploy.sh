96376b0a8d6348538bd8a1772b25b41fa5a60b93
mihilt <kwschic@gmail.com>
2023-09-01 19:15:34 +0900

    chore: check if new image works well
    - check it before stopping container for safety

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,27 @@ sleep 2
 echo "=> Build image..."
 docker build -t $IMAGE_NAME .
 
+# check if new image works well by running container
+echo "=> Check if new image works well by running container..."
+docker run -d -p 4001:4001 --name $IMAGE_NAME-temp $IMAGE_NAME
+
+sleep 5
+
+if docker ps -q --filter "name=$CONTAINER_NAME-temp" | grep -q .; then
+    echo "=> Node is running well."
+    docker stop $CONTAINER_NAME-temp
+    docker rm $CONTAINER_NAME-temp
+else
+    echo "=> Node is not running well."
+    echo "##############################################"
+    echo "             CHECK ERROR LOGS                "
+    echo "##############################################"
+    docker logs $CONTAINER_NAME-temp
+    echo "##############################################"
+    docker rm $CONTAINER_NAME-temp
+    exit 1
+fi
+
 # stop container
 echo "=> Stop container..."
 docker stop $CONTAINER_NAME
