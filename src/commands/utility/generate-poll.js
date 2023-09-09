c0ab7fcdf6ea165c4ff496622cfc48134ab630ea
mihilt <kwschic@gmail.com>
2023-09-09 17:42:29 +0900

    fix: clean up unnecessary things

--- src/commands/utility/generate-poll.js
+++ src/commands/utility/generate-poll.js
@@ -1,13 +1,11 @@
 import { SlashCommandBuilder } from 'discord.js';
-import channelLog, {
-  generateInteractionCreateLogContent,
-} from '../../service/utils/channel-log.js';
+import GeneratePollChatInputCommand from '../../service/interaction/is-chat-input-command/generate-poll.js';
 
 export default {
   data: new SlashCommandBuilder().setName('generate-poll').setDescription('Generate a poll'),
 
   // eslint-disable-next-line no-unused-vars, no-empty-function
   async execute(interaction) {
-    channelLog(generateInteractionCreateLogContent(interaction));
+    GeneratePollChatInputCommand(interaction);
   },
 };
