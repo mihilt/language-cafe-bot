b0446bf2ca22497647c3a30ff7344b211958e2fb
mihilt <kwschic@gmail.com>
2023-10-09 15:05:33 +0900

    feat: add pass-the-coffee-cup feature

---
+++ src/service/messageCreate/pass-the-coffee-cup.js
@@ -0,0 +1,6 @@
+import alphabet from '../../data/alphabet.js';
+
+export default async (message) => {
+  if (message.content.includes('<@')) return;
+  message.react(alphabet[Math.floor(Math.random() * alphabet.length)]);
+};
