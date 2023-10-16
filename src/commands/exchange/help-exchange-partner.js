b3b7492da1fed736777672a6576b94563edce65f
mihilt <kwschic@gmail.com>
2023-10-16 11:27:22 +0900

    fix: fix help-exchange-partner's content

--- src/commands/exchange/help-exchange-partner.js
+++ src/commands/exchange/help-exchange-partner.js
@@ -1,4 +1,4 @@
-import { SlashCommandBuilder, bold } from 'discord.js';
+import { SlashCommandBuilder, bold, channelMention } from 'discord.js';
 import channelLog, {
   generateInteractionCreateLogContent,
 } from '../../service/utils/channel-log.js';
@@ -15,11 +15,13 @@ export default {
       '/register-my-exchange-listing',
     )} in this channel, fill out the questions, then click Done (if you are learning a smaller language, first send ${bold(
       '/get-language-list',
-    )} to see what languages are are in our database. If it is not there, please request it to be added in #public-server-help)\n2. If it says invalid language, try again and make sure you spell everything correctly\n3. After you have submitted your listing, send ${bold(
+    )} to see what languages are are in our database. If it is not there, please request it to be added in ${channelMention(
+      '739915203096870917',
+    )})\n2. If it says invalid language, try again and make sure you spell everything correctly\n3. After you have submitted your listing, send ${bold(
       '/get-exchange-listing',
     )} to get your possible exchange partners (it will match your target language up with another person’s native language and sort by recent)\n4. If you would like to edit your listing, send ${bold(
-      '/register-my-exchange-listing again',
-    )}\n\nIf you would like to delete your exchange listing, send ${bold(
+      '/register-my-exchange-listing',
+    )} again\n5. If you would like to delete your exchange listing, send ${bold(
       '/delete-my-exchange-listing',
     )} to delete it`;
 
