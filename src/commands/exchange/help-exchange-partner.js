e37f85bd3451306e9b7370fe306afc77ba44df3d
mihilt <kwschic@gmail.com>
2023-10-20 20:33:46 +0900

    feat: add custom help sticky message

--- src/commands/exchange/help-exchange-partner.js
+++ src/commands/exchange/help-exchange-partner.js
@@ -11,7 +11,9 @@ export default {
   async execute(interaction) {
     channelLog(generateInteractionCreateLogContent(interaction));
 
-    const contents = `### New Find Exchange Partner Message Format\n\nPlease follow the following steps to be put in our exchange partner database so you can find a partner easier\n\n1. Send ${bold(
+    const title = 'Find Exchange Partner Message Format';
+
+    const description = `Please follow the following steps to be put in our exchange partner database so you can find a partner easier\n\n1. Send ${bold(
       '/register-my-exchange-listing',
     )} in this channel, fill out the questions, then click Done (if you are learning a smaller language, first send ${bold(
       '/get-language-list',
@@ -29,7 +31,11 @@ export default {
       embeds: [
         {
           color: 0x65a69e,
-          description: contents,
+          title,
+          description,
+          image: {
+            url: 'https://media.discordapp.net/attachments/1069942934381793330/1164765573327093810/ezgif.com-crop_1.gif?ex=65446778&is=6531f278&hm=f56cf6b2fc88b9720aa4b1e1154c8cf2e2429465e5d3b8e821af1741f0c47e6e&=&width=750&height=411',
+          },
         },
       ],
       ephemeral: true,
--- src/service/interaction/is-modal-submit/register-my-exchange-listing.js
+++ src/service/interaction/is-modal-submit/register-my-exchange-listing.js
@@ -1,6 +1,7 @@
-import { userMention } from 'discord.js';
+import { bold, channelMention, userMention } from 'discord.js';
 import languages from '../../../data/languages.js';
 import ExchangePartner from '../../../models/ExchangePartner.js';
+import config from '../../../config/index.js';
 
 const convertToProperCase = (input) => {
   const words = input.toLowerCase().split(' ');
@@ -42,6 +43,7 @@ export default async (interaction) => {
       ],
       ephemeral: true,
     });
+
     return;
   }
 
@@ -128,9 +130,58 @@ export default async (interaction) => {
           name: `${interaction.member.user.globalName}(${interaction.member.user.username}#${interaction.member.user.discriminator})`,
           icon_url: interaction.member.user.avatarURL(),
         },
-        footer: {
-          icon_url: interaction.guild.iconURL(),
-          text: 'use /help-exchange-partner command to register your own exchange partner listing',
+      },
+    ],
+  });
+
+  await interaction.followUp({
+    embeds: [
+      {
+        color: 0x65a69e,
+        title: 'You have successfully registered your language exchange partner listing.',
+        description:
+          '\nTry to find your exchange partner by using the `/get-exchange-listing` command.',
+      },
+    ],
+    ephemeral: true,
+  });
+
+  const channelId = interaction.channel.id;
+
+  const title = 'Find Exchange Partner Message Format';
+
+  const description = `Please follow the following steps to be put in our exchange partner database so you can find a partner easier\n\n1. Send ${bold(
+    '/register-my-exchange-listing',
+  )} in this channel, fill out the questions, then click Done (if you are learning a smaller language, first send ${bold(
+    '/get-language-list',
+  )} to see what languages are are in our database. If it is not there, please request it to be added in ${channelMention(
+    '739915203096870917',
+  )})\n2. If it says invalid language, try again and make sure you spell everything correctly\n3. After you have submitted your listing, send ${bold(
+    '/get-exchange-listing',
+  )} to get your possible exchange partners (it will match your target language up with another person’s native language and sort by recent)\n4. If you would like to edit your listing, send ${bold(
+    '/register-my-exchange-listing',
+  )} again\n5. If you would like to delete your exchange listing, send ${bold(
+    '/delete-my-exchange-listing',
+  )} to delete it`;
+
+  const currentMessages = await interaction.client.channels.cache
+    .get(channelId)
+    .messages.fetch({ limit: 50 });
+
+  const stickyMessage = currentMessages.find(
+    (message) => message?.author?.id === config.CLIENT_ID && message?.embeds[0]?.title === title,
+  );
+
+  await stickyMessage?.delete();
+
+  await interaction.client.channels.cache.get(channelId).send({
+    embeds: [
+      {
+        color: 0x65a69e,
+        title,
+        description,
+        image: {
+          url: 'https://media.discordapp.net/attachments/1069942934381793330/1164765573327093810/ezgif.com-crop_1.gif?ex=65446778&is=6531f278&hm=f56cf6b2fc88b9720aa4b1e1154c8cf2e2429465e5d3b8e821af1741f0c47e6e&=&width=750&height=411',
         },
       },
     ],
