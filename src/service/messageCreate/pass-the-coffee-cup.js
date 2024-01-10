aa8cdf7ec57c4adf3a5ef5e0008d1058ed5be1d8
mihilt <kwschic@gmail.com>
2024-01-10 23:02:36 +0900

    fix: change pass-the-coffee-cup's user enrollment method

--- src/service/messageCreate/pass-the-coffee-cup.js
+++ src/service/messageCreate/pass-the-coffee-cup.js
@@ -1,6 +1,21 @@
-import alphabet from '../../data/alphabet.js';
+import { userMention } from 'discord.js';
+import config from '../../config/index.js';
+
+const { PASS_THE_COFFEE_CUP_ENROLLMENT_MESSAGE_ID: enrollmentMessageId } = config;
 
 export default async (message) => {
-  if (message.content.includes('<@')) return;
-  message.react(alphabet[Math.floor(Math.random() * alphabet.length)]).catch(() => {});
+  const enrollmentMessage = await message.channel.messages.fetch(enrollmentMessageId);
+  const usersPromise = enrollmentMessage.reactions.cache.map((reaction) => reaction.users.fetch());
+
+  const usersCollection = await Promise.all(usersPromise);
+
+  const userIdArray = usersCollection
+    .map((userCollection) => userCollection.map((user) => user.id))
+    .flat();
+
+  const randomUserId = userIdArray[Math.floor(Math.random() * userIdArray.length)];
+
+  const content = `${userMention(randomUserId)} pass the coffee cup!`;
+
+  await message.reply(content);
 };
