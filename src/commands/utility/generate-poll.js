938f90baae4046156f434e6a216ac47898c53dc6
mihilt <kwschic@gmail.com>
2024-03-24 10:20:34 +0900

    fix: fix generate-poll permission

--- src/commands/utility/generate-poll.js
+++ src/commands/utility/generate-poll.js
@@ -1,12 +1,8 @@
-import { PermissionFlagsBits, SlashCommandBuilder } from 'discord.js';
+import { SlashCommandBuilder } from 'discord.js';
 import generatePollChatInputCommand from '../../service/interaction/is-chat-input-command/generate-poll.js';
 
 export default {
-  // only staff can use this command
-  data: new SlashCommandBuilder()
-    .setName('generate-poll')
-    .setDescription('Generate a poll')
-    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+  data: new SlashCommandBuilder().setName('generate-poll').setDescription('Generate a poll'),
 
   async execute(interaction) {
     generatePollChatInputCommand(interaction);
