ebc0c57126cb5ba663ed8ae7ad982728ed9cda3d
mihilt <kwschic@gmail.com>
2024-05-03 23:50:05 +0900

    fix: fix to react the last message even if the category is finished

--- src/service/messageCreate/categories.js
+++ src/service/messageCreate/categories.js
@@ -5,24 +5,24 @@ import Category from '../../models/category.js';
 import flagEmojis from '../../data/flag-emojis.js';
 
 export default async (message) => {
-  const { content: clientContent } = message;
-
-  if (!flagEmojis.some((flagEmoji) => clientContent.includes(flagEmoji))) {
-    return;
-  }
-
-  const clientAlphabet = latinize(clientContent)
-    .split('')
-    .filter((e) => /[a-zA-Z]/.test(e))[0]
-    ?.toUpperCase();
-
-  if (!clientAlphabet) {
-    return;
-  }
-
-  let currentCategory = await Category.findOne().sort({ createdAt: 1 });
-
   try {
+    const { content: clientContent } = message;
+
+    if (!flagEmojis.some((flagEmoji) => clientContent.includes(flagEmoji))) {
+      return;
+    }
+
+    const clientAlphabet = latinize(clientContent)
+      .split('')
+      .filter((e) => /[a-zA-Z]/.test(e))[0]
+      ?.toUpperCase();
+
+    if (!clientAlphabet) {
+      return;
+    }
+
+    let currentCategory = await Category.findOne().sort({ createdAt: 1 });
+
     if (!currentCategory) {
       message.channel.send({
         embeds: [
@@ -43,6 +43,20 @@ export default async (message) => {
       message.react('❌').catch(() => {});
       return;
     }
+
+    message.react(alphabetEmojis[clientAlphabet]).catch(() => {});
+
+    const title = 'Current Category';
+
+    const currentMessages = await message.channel.messages.fetch({ limit: 50 });
+
+    const stickyMessage = currentMessages.find(
+      (currentMessage) =>
+        currentMessage?.author?.id === config.CLIENT_ID &&
+        currentMessage?.embeds[0]?.title === title,
+    );
+
+    await stickyMessage?.delete().catch(() => {});
 
     let filteredCategoryAlphabet = currentCategoryAlphabet.replace(clientAlphabet, '');
 
@@ -74,6 +88,16 @@ export default async (message) => {
         return;
       }
 
+      await message.channel.send({
+        embeds: [
+          {
+            color: 0x65a69e,
+            title: 'New Category',
+            description: `\`\`\`${currentCategory.message}\`\`\``,
+          },
+        ],
+      });
+
       filteredCategoryAlphabet = currentCategory.alphabet;
     } else {
       await Category.updateOne(
@@ -84,33 +108,17 @@ export default async (message) => {
       );
     }
 
-    message.react(alphabetEmojis[clientAlphabet]).catch(() => {});
-
-    const title = 'Current Category';
-
-    const description = `\`\`\`${
-      currentCategory.message
-    }\`\`\`\n### Remaining Letters\n${filteredCategoryAlphabet
-      .split('')
-      .map((e) => alphabetEmojis[e])
-      .join(', ')}`;
-
-    const currentMessages = await message.channel.messages.fetch({ limit: 50 });
-
-    const stickyMessage = currentMessages.find(
-      (currentMessage) =>
-        currentMessage?.author?.id === config.CLIENT_ID &&
-        currentMessage?.embeds[0]?.title === title,
-    );
-
-    await stickyMessage?.delete().catch(() => {});
-
     await message.channel.send({
       embeds: [
         {
           color: 0x65a69e,
           title,
-          description,
+          description: `\`\`\`${
+            currentCategory.message
+          }\`\`\`\n### Remaining Letters\n${filteredCategoryAlphabet
+            .split('')
+            .map((e) => alphabetEmojis[e])
+            .join(', ')}`,
         },
       ],
     });
