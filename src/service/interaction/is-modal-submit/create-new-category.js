90a6a70dcd019a0cf34e1083339ee4addc6e6416
mihilt <kwschic@gmail.com>
2024-05-06 18:57:51 +0900

    fix: fix to handle exceptions

--- src/service/interaction/is-modal-submit/create-new-category.js
+++ src/service/interaction/is-modal-submit/create-new-category.js
@@ -1,43 +1,71 @@
 import Category from '../../../models/category.js';
 
 export default async (interaction) => {
-  const message = interaction.fields.getTextInputValue('message');
-  const exceptedLetters = interaction.fields.getTextInputValue('excepted-letters');
+  try {
+    const message = interaction.fields.getTextInputValue('message');
+    const exceptedLetters = interaction.fields.getTextInputValue('excepted-letters');
 
-  const refinedLetters = exceptedLetters
-    .toUpperCase()
-    .split('')
-    .filter((e) => /[a-zA-Z]/.test(e))
-    .join('');
+    const refinedLetters = exceptedLetters
+      .toUpperCase()
+      .split('')
+      .filter((e) => /[a-zA-Z]/.test(e))
+      .join('');
 
-  const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    .split('')
-    .filter((e) => !refinedLetters.includes(e))
-    .join('');
+    const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+      .split('')
+      .filter((e) => !refinedLetters.includes(e))
+      .join('');
 
-  const res = await Category.create({
-    message,
-    alphabet: letters,
-  });
+    if (letters === '') {
+      await interaction.reply({
+        embeds: [
+          {
+            color: 0x65a69e,
+            description: 'Failed to create category\n\nThere are no remaining letters',
+          },
+        ],
+        ephemeral: true,
+      });
 
-  if (res) {
-    await interaction.reply({
-      embeds: [
-        {
-          color: 0x65a69e,
-          description: `Category created successfully\n\nMessage\`\`\`\n${message}\n\`\`\`\nRemaining Letters\`\`\`\n${letters
-            .split('')
-            .join(',')}\n\`\`\``,
-        },
-      ],
-      ephemeral: true,
+      return;
+    }
+
+    const res = await Category.create({
+      message,
+      alphabet: letters,
     });
-  } else {
+
+    if (res) {
+      await interaction.reply({
+        embeds: [
+          {
+            color: 0x65a69e,
+            description: `Category created successfully\n\nMessage\`\`\`\n${message}\n\`\`\`\nRemaining Letters\`\`\`\n${letters
+              .split('')
+              .join(',')}\n\`\`\``,
+          },
+        ],
+        ephemeral: true,
+      });
+    } else {
+      await interaction.reply({
+        embeds: [
+          {
+            color: 0x65a69e,
+            description: 'Failed to create category',
+          },
+        ],
+        ephemeral: true,
+      });
+    }
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error);
     await interaction.reply({
       embeds: [
         {
           color: 0x65a69e,
-          description: 'Failed to create category',
+          description: 'Failed to create category (Internal Server Error)',
         },
       ],
       ephemeral: true,
