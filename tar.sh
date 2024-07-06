658b9ec7fa1f37f91cf89fa32303201ae778c6c3
mihilt <kwschic@gmail.com>
2024-07-06 11:36:58 +0900

    chore: fix trivial

--- tar.sh
+++ tar.sh
@@ -1,2 +1,16 @@
-tar -cf ../language-cafe-bot.tar --exclude=node_modules --exclude=.git --exclude=.vscode .
+OUTPUT_FILE="../language-cafe-bot.tar"
+
+echo "=> Creating tar file excluding patterns from .tarignore..."
+tar -cf $OUTPUT_FILE --exclude-from=.tarignore .
+
+if [ -f $OUTPUT_FILE ]; then
+    echo "=> Tar file created successfully: $OUTPUT_FILE"
+else
+    echo "=> Failed to create tar file."
+    exit 1
+fi
+
+echo "=> Opening directory containing tar file..."
 open ../
+
+echo "=> Done."
